bf22151adee3ba69ca4c40b16e44a67f3490c0c5
newyork_loki <137760120+lowkeyrossi@users.noreply.github.com>
2025-08-23 21:43:38 +0530

    Remove unwanted comments from abs source

--- src/abs/MN_abs.c
+++ src/abs/MN_abs.c
@@ -5,15 +5,13 @@
 
 /*
 Function Input:
-    dst   - Destination array that store results
-    src   - Source array that stores user input
+    dst   - Destination that store results
+    src   - Source that stores user input
     count - Count is an integer that stores number of elements
     fabsf - Calculates the absolute value of the floating-point argument. Refer to MSVC Docs(https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/fabsf-fabsff-fabsfl?view=msvc-170)
     abs   - Calculates the absolute value of the integer argument. Refer to MSVC Docs(https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/abs-labs-llabs-abs64?view=msvc-170)
  */
 
-//C Implementation of abs function for int32(Supports upto 4-Dimensional Array)
-//C Implementation for 1-Dimensional Array
 MN_result_t MN_abs_int_c (MN_int32_t * dst, MN_int32_t * src, MN_uint32_t count)
 {
     for ( unsigned int itr = 0; itr < count; itr++ )
@@ -23,7 +21,6 @@ MN_result_t MN_abs_int_c (MN_int32_t * dst, MN_int32_t * src, MN_uint32_t count)
     return MN_SUCCESS;
 }
 
-//C Implementation for 2-Dimensional Array
 MN_result_t MN_abs_vec2i_c (MN_vec2i_t * dst, MN_vec2i_t * src, MN_uint32_t count)
 {
     for ( unsigned int itr = 0; itr < count; itr++ )
@@ -34,7 +31,6 @@ MN_result_t MN_abs_vec2i_c (MN_vec2i_t * dst, MN_vec2i_t * src, MN_uint32_t coun
     return MN_SUCCESS;
 }
 
-//C Implementation for 3-Dimensional Array
 MN_result_t MN_abs_vec3i_c (MN_vec3i_t * dst, MN_vec3i_t * src, MN_uint32_t count)
 {
     for ( unsigned int itr = 0; itr < count; itr++ )
@@ -46,7 +42,6 @@ MN_result_t MN_abs_vec3i_c (MN_vec3i_t * dst, MN_vec3i_t * src, MN_uint32_t coun
     return MN_SUCCESS;
 }
 
-//C Implementation for 4-Dimensional Array
 MN_result_t MN_abs_vec4i_c (MN_vec4i_t * dst, MN_vec4i_t * src, MN_uint32_t count)
 {
     for ( unsigned int itr = 0; itr < count; itr++ )
@@ -59,8 +54,6 @@ MN_result_t MN_abs_vec4i_c (MN_vec4i_t * dst, MN_vec4i_t * src, MN_uint32_t coun
     return MN_SUCCESS;
 }
 
-//C Implementation of abs function for float32(Supports upto 4-Dimensional Array)
-//C Implementation for 1-Dimensional Array
 MN_result_t MN_abs_float_c (MN_float32_t * dst, MN_float32_t * src, MN_uint32_t count)
 {
     for ( unsigned int itr = 0; itr < count; itr++ )
@@ -70,7 +63,6 @@ MN_result_t MN_abs_float_c (MN_float32_t * dst, MN_float32_t * src, MN_uint32_t 
     return MN_SUCCESS;
 }
 
-//C Implementation for 2-Dimensional Array
 MN_result_t MN_abs_vec2f_c (MN_vec2f_t * dst, MN_vec2f_t * src, MN_uint32_t count)
 {
     for ( unsigned int itr = 0; itr < count; itr++ )
@@ -81,7 +73,6 @@ MN_result_t MN_abs_vec2f_c (MN_vec2f_t * dst, MN_vec2f_t * src, MN_uint32_t coun
     return MN_SUCCESS;
 }
 
-//C Implementation for 3-Dimensional Array
 MN_result_t MN_abs_vec3f_c (MN_vec3f_t * dst, MN_vec3f_t * src, MN_uint32_t count)
 {
     for ( unsigned int itr = 0; itr < count; itr++ )
@@ -93,7 +84,6 @@ MN_result_t MN_abs_vec3f_c (MN_vec3f_t * dst, MN_vec3f_t * src, MN_uint32_t coun
     return MN_SUCCESS;
 }
 
-//C Implementation for 4-Dimensional Array
 MN_result_t MN_abs_vec4f_c (MN_vec4f_t * dst, MN_vec4f_t * src, MN_uint32_t count)
 {
     for ( unsigned int itr = 0; itr < count; itr++ )
