68a7d53cecdf19360648f0be3726b7736b8232b9
newyork_loki <137760120+lowkeyrossi@users.noreply.github.com>
2025-08-24 10:13:00 +0530

    use lowercase for defining data types

--- includes/MN_dtype.h
+++ includes/MN_dtype.h
@@ -11,10 +11,10 @@
 //-------- Data Types used in MATHNEON Project ---------
 /////////////////////////////////////////////////////////
 
-typedef int      MN_result_t;
-typedef int32_t  MN_int32_t;
-typedef float    MN_float32_t;
-typedef uint32_t MN_uint32_t;
+typedef int      mn_result_t;
+typedef int32_t  mn_int32_t;
+typedef float    mn_float32_t;
+typedef uint32_t mn_uint32_t;
 
 /////////////////////////////////////////////////////////
 // Return values to capture the state of program
@@ -23,63 +23,63 @@ typedef uint32_t MN_uint32_t;
 #define MN_ERROR -1
 
 /**
- * @brief A 2-tuple of MN_int32_t values.
+ * @brief A 2-tuple of mn_int32_t values.
  */
 typedef struct
 {
-    MN_int32_t x;
-    MN_int32_t y;
-} MN_vec2i_t;
+    mn_int32_t x;
+    mn_int32_t y;
+} mn_vec2i_t;
 
 /**
- * @brief A 3-tuple of MN_int32_t values.
+ * @brief A 3-tuple of mn_int32_t values.
  */
 typedef struct
 {
-    MN_int32_t x;
-    MN_int32_t y;
-    MN_int32_t z;
-} MN_vec3i_t;
+    mn_int32_t x;
+    mn_int32_t y;
+    mn_int32_t z;
+} mn_vec3i_t;
 
 /**
- * @brief A 4-tuple of MN_int32_t values.
+ * @brief A 4-tuple of mn_int32_t values.
  */
 typedef struct
 {
-    MN_int32_t x;
-    MN_int32_t y;
-    MN_int32_t z;
-    MN_int32_t w;
-} MN_vec4i_t;
+    mn_int32_t x;
+    mn_int32_t y;
+    mn_int32_t z;
+    mn_int32_t w;
+} mn_vec4i_t;
 
 /**
- * @brief A 2-tuple of MN_float32_t values.
+ * @brief A 2-tuple of mn_float32_t values.
  */
 typedef struct
 {
-    MN_float32_t x;
-    MN_float32_t y;
-} MN_vec2f_t;
+    mn_float32_t x;
+    mn_float32_t y;
+} mn_vec2f_t;
 
 /**
- * @brief A 3-tuple of MN_float32_t values.
+ * @brief A 3-tuple of mn_float32_t values.
  */
 typedef struct
 {
-    MN_float32_t x;
-    MN_float32_t y;
-    MN_float32_t z;
-} MN_vec3f_t;
+    mn_float32_t x;
+    mn_float32_t y;
+    mn_float32_t z;
+} mn_vec3f_t;
 
 /**
- * @brief A 4-tuple of MN_float32_t values.
+ * @brief A 4-tuple of mn_float32_t values.
  */
 typedef struct
 {
-    MN_float32_t x;
-    MN_float32_t y;
-    MN_float32_t z;
-    MN_float32_t w;
-} MN_vec4f_t;
+    mn_float32_t x;
+    mn_float32_t y;
+    mn_float32_t z;
+    mn_float32_t w;
+} mn_vec4f_t;
 
 #endif // MN_DTYPES_H
