812712737008436ae841f4ede4c82f09233a7ac6
newyork_loki <137760120+lowkeyrossi@users.noreply.github.com>
2025-08-24 10:58:55 +0530

    Rename assert for abs

--- includes/MN_macro.h
+++ includes/MN_macro.h
@@ -3,10 +3,10 @@
 // A few macros to check pointers and their address range to make sure there's
 // no unwanted overlap between any two of them 
 
-#define MN_CHECKPOINTER_DstSrcCst \
+#define MN_CHECK_DstSRC \
    if ( (void *)dst < (void *)src ) \                         
     { assert ( (void *)dst + count <= (void *)src ); } \
    else if ( (void *)dst > (void *)src ) \
     { assert ( (void *)src + count <= (void *)dst ); }
 
-#define MN_CHECKPOINTER_DstSrc MN_CHECKPOINTER_DstSrcCst
+#define MN_ASSERT_DS MN_CHECK_DstSRC
