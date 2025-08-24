cd289ea005730965fd92ba0c116c906330130573
newyork_loki <137760120+lowkeyrossi@users.noreply.github.com>
2025-08-24 11:06:57 +0530

    Fix macro definition for MN_CHECK

--- includes/MN_macro.h
+++ includes/MN_macro.h
@@ -1,12 +1,13 @@
-
-
 // A few macros to check pointers and their address range to make sure there's
 // no unwanted overlap between any two of them 
 
 #define MN_CHECK_DstSRC \
-   if ( (void *)dst < (void *)src ) \                         
-    { assert ( (void *)dst + count <= (void *)src ); } \
-   else if ( (void *)dst > (void *)src ) \
-    { assert ( (void *)src + count <= (void *)dst ); }
+    do { \
+        if ((void *)dst < (void *)src) { \
+            assert((void *)dst + count <= (void *)src); \
+        } else if ((void *)dst > (void *)src) { \
+            assert((void *)src + count <= (void *)dst); \
+        } \
+    } while (0)
 
 #define MN_ASSERT_DS MN_CHECK_DstSRC
