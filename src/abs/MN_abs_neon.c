2533dec7dec7ea11183315aa2f6ed2702e02004c
newyork_loki <137760120+lowkeyrossi@users.noreply.github.com>
2025-08-24 18:30:42 +0530

    Improve description for abs routines

--- src/abs/MN_abs_neon.c
+++ src/abs/MN_abs_neon.c
@@ -11,11 +11,18 @@ Input Arguments:
     count  - Number of elements to process
 
 Intrinsics used: 
-    vld1q_f32 - NEON intrinsic to load 4 float32 values from memory
-    vabsq_f32 - NEON intrinsic to compute absolute value of 4 float32 values in parallel
-    vst1q_f32 - NEON intrinsic to store 4 float32 values back to memory
-    fabsf - Standard C library function that calculates the absolute value of a single float.
-            Used here for leftover elements when count is not a multiple of 4.
+    float32 routines:
+        vld1q_f32 - NEON intrinsic to load 4 float32 values from memory
+        vabsq_f32 - NEON intrinsic to compute absolute value of 4 float32 values in parallel
+        vst1q_f32 - NEON intrinsic to store 4 float32 values back to memory
+        fabsf - Standard C library function that calculates the absolute value of a single float.
+                Used here for leftover elements when count is not a multiple of 4.
+    int32 routines:
+        vld1q_s32 - NEON intrinsic to load 4 int32 values from memory
+        vabsq_s32 - NEON intrinsic to compute absolute value of 4 int32 values in parallel
+        vst1q_s32 - NEON intrinsic to store 4 int32 values back to memory
+        fabs - Standard C library function that calculates the absolute value of a single integer.
+                Used here for leftover elements when count is not a multiple of 4.
 */
 
 mn_result_t mn_abs_float_neon(mn_float32_t * dst, mn_float32_t * src, mn_uint32_t count)
