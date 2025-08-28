d5dc1096da4d9ade7ea28862789ac7931a164dee
Green Committer <137760120+Greenie0701@users.noreply.github.com>
2025-08-28 09:12:53 +0530

    Extern C  implementation of Add operator

--- includes/MN_math.h
+++ includes/MN_math.h
@@ -5,12 +5,13 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 /**
+ * ================================
  * MN ABSOLUTE OPERATOR USING C & NEON
- * @param dst   Destination buffer (must not overlap with src).
- * @param src   Source buffer.
- * @param count Number of elements.
+ * ================================
  */
+
 /**
  * @brief Computes absolute value of float array using C.
  */
@@ -18,6 +19,7 @@ mn_result_t mn_abs_float_c(mn_float32_t *dst, mn_float32_t *src, size_t count);
 mn_result_t mn_abs_vec2f_c(mn_vec2f_t *dst, mn_vec2f_t *src, size_t count);
 mn_result_t mn_abs_vec3f_c(mn_vec3f_t *dst, mn_vec3f_t *src, size_t count);
 mn_result_t mn_abs_vec4f_c(mn_vec4f_t *dst, mn_vec4f_t *src, size_t count);
+
 /**
  * @brief Computes absolute value of float array using NEON.
  */
@@ -25,6 +27,7 @@ mn_result_t mn_abs_float_neon(mn_float32_t *dst, mn_float32_t *src, size_t count
 mn_result_t mn_abs_vec2f_neon(mn_vec2f_t *dst, mn_vec2f_t *src, size_t count);
 mn_result_t mn_abs_vec3f_neon(mn_vec3f_t *dst, mn_vec3f_t *src, size_t count);
 mn_result_t mn_abs_vec4f_neon(mn_vec4f_t *dst, mn_vec4f_t *src, size_t count);
+
 /**
  * @brief Computes absolute value of int array using C.
  */
@@ -32,6 +35,7 @@ mn_result_t mn_abs_int32_c(mn_int32_t *dst, mn_int32_t *src, size_t count);
 mn_result_t mn_abs_vec2i_c(mn_vec2i_t *dst, mn_vec2i_t *src, size_t count);
 mn_result_t mn_abs_vec3i_c(mn_vec3i_t *dst, mn_vec3i_t *src, size_t count);
 mn_result_t mn_abs_vec4i_c(mn_vec4i_t *dst, mn_vec4i_t *src, size_t count);
+
 /**
  * @brief Computes absolute value of int array using NEON.
  */
@@ -39,6 +43,29 @@ mn_result_t mn_abs_int32_neon(mn_int32_t *dst, mn_int32_t *src, size_t count);
 mn_result_t mn_abs_vec2i_neon(mn_vec2i_t *dst, mn_vec2i_t *src, size_t count);
 mn_result_t mn_abs_vec3i_neon(mn_vec3i_t *dst, mn_vec3i_t *src, size_t count);
 mn_result_t mn_abs_vec4i_neon(mn_vec4i_t *dst, mn_vec4i_t *src, size_t count);
+
+/**
+ * ================================
+ * MN ADD OPERATOR USING C
+ * ================================
+ */
+
+/**
+ * @brief Adds two int arrays.
+ */
+mn_result_t mn_add_int32_c(mn_int32_t *dst, mn_int32_t *src1, mn_int32_t *src2, mn_uint32_t count);
+mn_result_t mn_add_vec2i_c(mn_vec2i_t *dst, mn_vec2i_t *src1, mn_vec2i_t *src2, mn_uint32_t count);
+mn_result_t mn_add_vec3i_c(mn_vec3i_t *dst, mn_vec3i_t *src1, mn_vec3i_t *src2, mn_uint32_t count);
+mn_result_t mn_add_vec4i_c(mn_vec4i_t *dst, mn_vec4i_t *src1, mn_vec4i_t *src2, mn_uint32_t count);
+
+/**
+ * @brief Adds two float arrays.
+ */
+mn_result_t mn_add_float_c(mn_float32_t *dst, mn_float32_t *src1, mn_float32_t *src2, mn_uint32_t count);
+mn_result_t mn_add_vec2f_c(mn_vec2f_t *dst, mn_vec2f_t *src1, mn_vec2f_t *src2, mn_uint32_t count);
+mn_result_t mn_add_vec3f_c(mn_vec3f_t *dst, mn_vec3f_t *src1, mn_vec3f_t *src2, mn_uint32_t count);
+mn_result_t mn_add_vec4f_c(mn_vec4f_t *dst, mn_vec4f_t *src1, mn_vec4f_t *src2, mn_uint32_t count);
+
 #ifdef __cplusplus
 }
 #endif
