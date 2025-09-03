028ad8a62239a12962d27b4d032c82ec0d59fb4c
Wise Man <137760120+Greenie0701@users.noreply.github.com>
2025-09-03 17:47:30 +0530

    Add C implementation for MN addc operator

--- src/addc/MN_addc.c
+++ src/addc/MN_addc.c
@@ -47,3 +47,49 @@ mn_result_t mn_addc_vec4f_c (mn_vec4f_t * dst, mn_vec4f_t * src, const mn_vec4f_
     }
     return MN_OK;
 }
+
+mn_result_t mn_addc_int_c (mn_int32_t * dst, mn_int32_t * src, const mn_int32_t cst, mn_uint32_t count)
+{
+    MN_ASSERT_DS;
+    for ( unsigned int itr = 0; itr < count; itr++ )
+    {
+        dst[ itr ] = src[ itr ] + cst;
+    }
+    return MN_OK;
+}
+
+mn_result_t mn_addc_vec2i_c (mn_vec2i_t * dst, mn_vec2i_t * src, const mn_vec2i_t * cst, mn_uint32_t count)
+{
+    MN_ASSERT_DS;
+    for ( unsigned int itr = 0; itr < count; itr++ )
+    {
+        dst[ itr ].x = src[ itr ].x + cst->x;
+        dst[ itr ].y = src[ itr ].y + cst->y;
+    }
+    return MN_OK;
+}
+
+mn_result_t mn_addc_vec3i_c (mn_vec3i_t * dst, mn_vec3i_t * src, const mn_vec3i_t * cst, mn_uint32_t count)
+{
+    MN_ASSERT_DS;
+    for ( unsigned int itr = 0; itr < count; itr++ )
+    {
+        dst[ itr ].x = src[ itr ].x + cst->x;
+        dst[ itr ].y = src[ itr ].y + cst->y;
+        dst[ itr ].z = src[ itr ].z + cst->z;
+    }
+    return MN_OK;
+}
+
+mn_result_t mn_addc_vec4i_c (mn_vec4i_t * dst, mn_vec4i_t * src, const mn_vec4i_t * cst, mn_uint32_t count)
+{
+    MN_ASSERT_DS;
+    for ( unsigned int itr = 0; itr < count; itr++ )
+    {
+        dst[ itr ].x = src[ itr ].x + cst->x;
+        dst[ itr ].y = src[ itr ].y + cst->y;
+        dst[ itr ].z = src[ itr ].z + cst->z;
+        dst[ itr ].w = src[ itr ].w + cst->w;
+    }
+    return MN_OK;
+}
