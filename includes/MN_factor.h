0b60f710f5db51396aa28747badb32c1670aa0d5
Wise Man <137760120+Greenie0701@users.noreply.github.com>
2025-09-06 21:44:57 +0530

    Add neon factor code multiply operator

--- includes/MN_factor.h
+++ includes/MN_factor.h
@@ -1501,6 +1501,280 @@ extern "C" {
    return res; \
   }
 
+
+#define MN_MUL_DstSrc1Src2_DO_COUNT_TIMES_FLOAT_NEON(loopCode1, loopCode2) { \
+    MN_CHECK_Dst1SRC1SRC2(dst, src1, src2); /* check dst/src1/src2 pointers don't overlap */ \
+    mn_result_t res = MN_OK; \
+    float32x4_t n_src1, n_src2; \
+    float32x4_t n_dst; \
+    float32x2_t n_rest1, n_rest2, n_rest; \
+    int dif = count % 4; /* finds remaining elements if not multiple of 4 */ \
+    for (; count > dif; count -= 4) { \
+        loopCode1; \
+    } \
+    if (dif != 0) { \
+        for (unsigned int idx = 0; idx < dif; idx++) { \
+            loopCode2; \
+        } \
+    } \
+    return res; \
+}
+
+#define MN_MUL_DstSrc1Src2_DO_COUNT_TIMES_INT32_NEON(loopCode1, loopCode2) { \
+    MN_CHECK_Dst1SRC1SRC2(dst, src1, src2); /* check dst/src1/src2 pointers don't overlap */ \
+    mn_result_t res = MN_OK; \
+    int32x4_t n_src1, n_src2; \
+    int32x4_t n_dst; \
+    int32x2_t n_rest1, n_rest2, n_rest; \
+    int dif = count % 4; /* finds remaining elements if not multiple of 4 */ \
+    for (; count > dif; count -= 4) { \
+        loopCode1; \
+    } \
+    if (dif != 0) { \
+        for (unsigned int idx = 0; idx < dif; idx++) { \
+            loopCode2; \
+        } \
+    } \
+    return res; \
+}
+
+// Vector operation macros (similar to the ABS pattern)
+#define MN_MUL_DstSrc1Src2_OPERATION_VEC2F_NEON(loopCode1, loopCode2) { \
+   mn_result_t res = MN_OK; \
+   float32x4_t n_src1, n_src2; \
+   float32x4_t n_dst; \
+   float32x2_t n_rest1, n_rest2, n_rest; \
+   int dif = count % 2; \
+   for (; count > dif; count -= 2) { \
+    loopCode1; \
+   } \
+   if ( 0 != dif ) { \
+    loopCode2; \
+   } \
+   return res; \
+}
+
+#define MN_MUL_DstSrc1Src2_OPERATION_VEC2I_NEON(loopCode1, loopCode2) { \
+   mn_result_t res = MN_OK; \
+   int32x4_t n_src1, n_src2; \
+   int32x4_t n_dst; \
+   int32x2_t n_rest1, n_rest2, n_rest; \
+   int dif = count % 2; \
+   for (; count > dif; count -= 2) { \
+    loopCode1; \
+   } \
+   if ( 0 != dif ) { \
+    loopCode2; \
+   } \
+   return res; \
+}
+
+#define MN_MUL_DstSrc1Src2_OPERATION_VEC3F_NEON(loopCode1, loopCode2) { \
+  mn_result_t res = MN_OK; \
+  float32x4_t n_src11, n_src12, n_src13; \
+  float32x4_t n_src21, n_src22, n_src23; \
+  float32x4_t n_dst1, n_dst2, n_dst3; \
+  int dif = count % 4;  \
+  for (; count > dif; count -= 4) { \
+   loopCode1; \
+  } \
+  if ( 0 != dif ) { \
+    unsigned int idx; \
+    for ( idx = 0 ; idx < dif; idx++ ) { \
+      loopCode2; \
+    } \
+  } \
+  return res; \
+}
+
+#define MN_MUL_DstSrc1Src2_OPERATION_VEC3I_NEON(loopCode1, loopCode2) { \
+  mn_result_t res = MN_OK; \
+  int32x4_t n_src11, n_src12, n_src13; \
+  int32x4_t n_src21, n_src22, n_src23; \
+  int32x4_t n_dst1, n_dst2, n_dst3; \
+  int dif = count % 4;  \
+  for (; count > dif; count -= 4) { \
+   loopCode1; \
+  } \
+  if ( 0 != dif ) { \
+    unsigned int idx; \
+    for ( idx = 0 ; idx < dif; idx++ ) { \
+      loopCode2; \
+    } \
+  } \
+  return res; \
+}
+
+#define MN_MUL_DstSrc1Src2_OPERATION_VEC4F_NEON(loopCode) { \
+   mn_result_t res = MN_OK; \
+   float32x4_t n_src1, n_src2; \
+   float32x4_t n_dst; \
+   for (; count != 0; count --) { \
+     loopCode; \
+    } \
+   return res; \
+}
+
+#define MN_MUL_DstSrc1Src2_OPERATION_VEC4I_NEON(loopCode) { \
+   mn_result_t res = MN_OK; \
+   int32x4_t n_src1, n_src2; \
+   int32x4_t n_dst; \
+   for (; count != 0; count --) { \
+     loopCode; \
+    } \
+   return res; \
+}
+
+// MAINLOOP macros for multiply (load -> mul -> store -> advance pointers)
+#define MN_MAINLOOP_FLOAT_NEON_MUL { \
+    n_src1 = vld1q_f32((float32_t*)src1); /* load 4 floats from src1 */ \
+    n_src2 = vld1q_f32((float32_t*)src2); /* load 4 floats from src2 */ \
+    n_dst = vmulq_f32(n_src1, n_src2);    /* compute multiply */ \
+    vst1q_f32((float32_t*)dst, n_dst);    /* store back */ \
+    src1 += 4; \
+    src2 += 4; \
+    dst += 4; \
+}
+
+#define MN_MAINLOOP_INT32_NEON_MUL { \
+    n_src1 = vld1q_s32((int32_t*)src1); /* load 4 int32 from src1 */ \
+    n_src2 = vld1q_s32((int32_t*)src2); /* load 4 int32 from src2 */ \
+    n_dst = vmulq_s32(n_src1, n_src2);  /* compute multiply */ \
+    vst1q_s32((int32_t*)dst, n_dst);    /* store back */ \
+    src1 += 4; \
+    src2 += 4; \
+    dst += 4; \
+}
+
+#define MN_MUL_DstSrc1Src2_MAINLOOP_VEC2F_NEON(loopCode) { \
+     n_src1 = vld1q_f32( (float32_t*)src1 ); /* load two vec2f (8 floats) */ \
+     n_src2 = vld1q_f32( (float32_t*)src2 ); /* load two vec2f (8 floats) */ \
+     src1 += 2; /* move to the next two vectors */ \
+     src2 += 2; /* move to the next two vectors */ \
+     loopCode; /* actual operation */ \
+     vst1q_f32 ( (float32_t*)dst , n_dst ); /* store back */ \
+     dst += 2; /* move to the next 2 vectors */ \
+}
+
+#define MN_MUL_DstSrc1Src2_MAINLOOP_VEC2I_NEON(loopCode) { \
+     n_src1 = vld1q_s32( (int32_t*)src1 ); /* load two vec2i */ \
+     n_src2 = vld1q_s32( (int32_t*)src2 ); /* load two vec2i */ \
+     src1 += 2; /* move to the next two vectors */ \
+     src2 += 2; /* move to the next two vectors */ \
+     loopCode; /* actual operation */ \
+     vst1q_s32 ( (int32_t*)dst , n_dst ); /* store back */ \
+     dst += 2; /* move to the next 2 vectors */ \
+}
+
+#define MN_MUL_DstSrc1Src2_MAINLOOP_VEC3F_NEON(loopCode) {                             \
+    n_src11 = vld1q_f32((float32_t*)src1);                                             \
+    src1 = (mn_vec3f_t*)((char*)src1 + 4 * sizeof(mn_float32_t));                      \
+    n_src12 = vld1q_f32((float32_t*)src1);                                             \
+    src1 = (mn_vec3f_t*)((char*)src1 + 4 * sizeof(mn_float32_t));                      \
+    n_src13 = vld1q_f32((float32_t*)src1);                                             \
+    src1 = (mn_vec3f_t*)((char*)src1 + 4 * sizeof(mn_float32_t));                      \
+    n_src21 = vld1q_f32((float32_t*)src2);                                             \
+    src2 = (mn_vec3f_t*)((char*)src2 + 4 * sizeof(mn_float32_t));                      \
+    n_src22 = vld1q_f32((float32_t*)src2);                                             \
+    src2 = (mn_vec3f_t*)((char*)src2 + 4 * sizeof(mn_float32_t));                      \
+    n_src23 = vld1q_f32((float32_t*)src2);                                             \
+    src2 = (mn_vec3f_t*)((char*)src2 + 4 * sizeof(mn_float32_t));                      \
+    loopCode;                                                                          \
+    vst1q_f32((float32_t*)dst, n_dst1);                                                \
+    dst = (mn_vec3f_t*)((char*)dst + 4 * sizeof(mn_float32_t));                        \
+    vst1q_f32((float32_t*)dst, n_dst2);                                                \
+    dst = (mn_vec3f_t*)((char*)dst + 4 * sizeof(mn_float32_t));                        \
+    vst1q_f32((float32_t*)dst, n_dst3);                                                \
+    dst = (mn_vec3f_t*)((char*)dst + 4 * sizeof(mn_float32_t));                        \
+}
+
+#define MN_MUL_DstSrc1Src2_MAINLOOP_VEC3I_NEON(loopCode) {                             \
+    n_src11 = vld1q_s32((int32_t*)src1);                                               \
+    src1 = (mn_vec3i_t*)((char*)src1 + 4 * sizeof(mn_int32_t));                        \
+    n_src12 = vld1q_s32((int32_t*)src1);                                               \
+    src1 = (mn_vec3i_t*)((char*)src1 + 4 * sizeof(mn_int32_t));                        \
+    n_src13 = vld1q_s32((int32_t*)src1);                                               \
+    src1 = (mn_vec3i_t*)((char*)src1 + 4 * sizeof(mn_int32_t));                        \
+    n_src21 = vld1q_s32((int32_t*)src2);                                               \
+    src2 = (mn_vec3i_t*)((char*)src2 + 4 * sizeof(mn_int32_t));                        \
+    n_src22 = vld1q_s32((int32_t*)src2);                                               \
+    src2 = (mn_vec3i_t*)((char*)src2 + 4 * sizeof(mn_int32_t));                        \
+    n_src23 = vld1q_s32((int32_t*)src2);                                               \
+    src2 = (mn_vec3i_t*)((char*)src2 + 4 * sizeof(mn_int32_t));                        \
+    loopCode;                                                                          \
+    vst1q_s32((int32_t*)dst, n_dst1);                                                  \
+    dst = (mn_vec3i_t*)((char*)dst + 4 * sizeof(mn_int32_t));                          \
+    vst1q_s32((int32_t*)dst, n_dst2);                                                  \
+    dst = (mn_vec3i_t*)((char*)dst + 4 * sizeof(mn_int32_t));                          \
+    vst1q_s32((int32_t*)dst, n_dst3);                                                  \
+    dst = (mn_vec3i_t*)((char*)dst + 4 * sizeof(mn_int32_t));                          \
+}
+
+#define MN_MUL_DstSrc1Src2_MAINLOOP_VEC4F_NEON(loopCode) { \
+     n_src1 = vld1q_f32( (float32_t*)src1 ); \
+     n_src2 = vld1q_f32( (float32_t*)src2 ); \
+     src1 ++; \
+     src2 ++; \
+     loopCode; \
+     vst1q_f32 ( (float32_t*)dst , n_dst ); \
+     dst ++; \
+}
+
+#define MN_MUL_DstSrc1Src2_MAINLOOP_VEC4I_NEON(loopCode) { \
+     n_src1 = vld1q_s32( (int32_t*)src1 ); \
+     n_src2 = vld1q_s32( (int32_t*)src2 ); \
+     src1 ++; \
+     src2 ++; \
+     loopCode; \
+     vst1q_s32 ( (int32_t*)dst , n_dst ); \
+     dst ++; \
+}
+
+// SECONDLOOP macros for multiply (handles leftover elements)
+#define MN_SECONDLOOP_FLOAT_MUL { \
+    *dst++ = *src1++ + *src2++; \
+}
+
+#define MN_SECONDLOOP_INT32_MUL { \
+    *dst++ = *src1++ + *src2++; \
+}
+
+#define MN_MUL_DstSrc1Src2_SECONDLOOP_VEC2F_NEON(loopCode) { \
+     n_rest1 = vld1_f32( (float32_t*)src1 ); \
+     n_rest2 = vld1_f32( (float32_t*)src2 ); \
+     loopCode; /* exceptional cases where the count isn't a multiple of 2 */ \
+     vst1_f32( (float32_t*)dst, n_rest); \
+}
+
+#define MN_MUL_DstSrc1Src2_SECONDLOOP_VEC2I_NEON(loopCode) { \
+     n_rest1 = vld1_s32( (int32_t*)src1 ); \
+     n_rest2 = vld1_s32( (int32_t*)src2 ); \
+     loopCode; /* exceptional cases where the count isn't a multiple of 2 */ \
+     vst1_s32( (int32_t*)dst, n_rest); \
+}
+
+#define MN_MUL_DstSrc1Src2_SECONDLOOP_VEC3F_NEON(loopCode) { \
+     float32x2x3_t n_rest1, n_rest2; \
+     n_rest1 = vld3_f32( (float32_t*)src1 ); \
+     n_rest2 = vld3_f32( (float32_t*)src2 ); \
+     loopCode; /* exceptional cases where the count isn't a multiple of 4 */ \
+     vst3_f32( (float32_t*)dst, n_rest1); /* store result */ \
+     src1++; \
+     src2++; \
+     dst++; \
+}
+
+#define MN_MUL_DstSrc1Src2_SECONDLOOP_VEC3I_NEON(loopCode) { \
+     int32x2x3_t n_rest1, n_rest2; \
+     n_rest1 = vld3_s32( (int32_t*)src1 ); \
+     n_rest2 = vld3_s32( (int32_t*)src2 ); \
+     loopCode; /* exceptional cases where the count isn't a multiple of 4 */ \
+     vst3_s32( (int32_t*)dst, n_rest1); /* store result */ \
+     src1++; \
+     src2++; \
+     dst++; \
+}
+
 // -----------------------------------------------------------------------------
 // End of header guards
 // -----------------------------------------------------------------------------
